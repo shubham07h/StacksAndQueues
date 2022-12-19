d1f36e301ee7390a8a2219c1be3f82e5fbccd6b9
shubham07h <shubham1609sharma@gmail.com>
2022-12-19 17:38:27 +0530

    commit message

---
+++ StacksAndQueues/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StacksandQueues
+{
+    internal class Node
+    {
+
+        public int data;
+        public Node Next;
+        public Node(int data)
+        {
+            this.data = data;
+            Next = null;
+        }
+    }
+}
+
---
+++ StacksAndQueues/Stack_Ex.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StacksAndQueues
+{
+    internal class Stack_Ex
+    {
+        Node top;
+        public void Stack_in()
+        {
+            this.top = null;
+        }
+        public void Push(int data)
+        {
+            Node node = new Node(data);
+            if (top == null)
+            {
+                node.Next = null;
+            }
+            else
+            {
+                node.Next = this.top;
+            }
+            this.top = node;
+            Console.WriteLine("{0} pushed to stack", data);
+        }
+        public void Display()
+        {
+            Node temp = this.top;
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.Next;
+            }
+        }
+    }
+}
